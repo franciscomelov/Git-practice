42f58abcb6be594527c89dd722c349a6cd3b8359
Francisco Ecatl Melo Valle <femva3012@hotmail.com>
2020-02-27 17:18:54 -0600

    problem 3: se añadio descripccion

--- FCC_projectEuler/Problem3_Largest_prime_factor.js
+++ FCC_projectEuler/Problem3_Largest_prime_factor.js
@@ -1,3 +1,8 @@
+//Project Euler: Problem 3: Largest prime factorPassed
+//The prime factors of 13195 are 5, 7, 13 and 29.
+
+//What is the largest prime factor of the given number?
+
 //https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-3-largest-prime-factor
 function largestPrimeFactor(number) {
   // Good luck!
