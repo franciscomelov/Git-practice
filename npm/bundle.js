b757ccd5ba58937aae97d9a44b7124d41acc7d8f
franciscomelov <femva3012@hotmail.com>
2020-03-03 11:49:39 -0600

    script added to package.json automate browserify

--- npm/bundle.js
+++ npm/bundle.js
@@ -17121,5 +17121,5 @@ var _ = require("lodash");
 console.log(_)
 
 var array = [1,2,3,4,5,6,7];
-console.log("answer",_.without(array,3))
+console.log("answer",_.without(array,1))
 },{"lodash":1}]},{},[2]);
--- npm/script.js
+++ npm/script.js
@@ -4,4 +4,4 @@ var _ = require("lodash");
 console.log(_)
 
 var array = [1,2,3,4,5,6,7];
-console.log("answer",_.without(array,3))
+console.log("answer",_.without(array,1))
